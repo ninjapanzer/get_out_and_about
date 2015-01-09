04db285d6c6015efaf39e6522c1ac6c8b77286cc
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-01-09 16:40:19 -0500

    Added simple calendar

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ActiveRecord::Base
+  extend SimpleCalendar
+  has_calendar :attribute => :start
+
   belongs_to :user
 
+  validates :title, :location, :overview, presence: true
+
+  def name
+    title
+  end
 
 end
