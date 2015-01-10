5abc701d4a8cbd82e7dbeadd81127d740c8cd2e7
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-01-10 10:44:11 -0500

    Devise user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109210244) do
+ActiveRecord::Schema.define(version: 20150110152257) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -31,5 +31,33 @@ ActiveRecord::Schema.define(version: 20150109210244) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.boolean  "verified",               default: false
+    t.text     "contact_info"
+    t.string   "organization"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
